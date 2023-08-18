8cc3a7b4e5db54a18b0fb001a1662ca1a6b95b71
akashsarkerbd <141321007+akashsarkerbd@users.noreply.github.com>
2023-08-19 01:57:37 +0600

    Update assignment.dart

--- assignment.dart
+++ assignment.dart
@@ -3,7 +3,6 @@ class Car {
   String model;
   int year;
   double milesDriven;
-
   Car(this.brand, this.model, this.year) : milesDriven = 0;
 
   void drive(double miles) {
@@ -29,13 +28,12 @@ class Car {
   int getAge() {
     return DateTime.now().year - year;
   }
-
+  
   static int numberOfCars = 0;
 }
 
 void main() {
   Car.numberOfCars = 0;
-
   Car car1 = Car('Toyota', 'Camry', 2020);
   car1.drive(10000);
 
@@ -47,10 +45,11 @@ void main() {
 
   Car.numberOfCars += 3;
 
-  print(
-      'Car 1: ${car1.getBrand()} ${car1.getModel()} ${car1.getYear()} Miles: ${car1.getMilesDriven().toStringAsFixed(0)} Age: ${car1.getAge()}');
-  print(
-      'Car 2: ${car2.getBrand()} ${car2.getModel()} ${car2.getYear()} Miles: ${car2.getMilesDriven().toStringAsFixed(0)} Age: ${car2.getAge()}');
-  print(
-      'Car 3: ${car3.getBrand()} ${car3.getModel()} ${car3.getYear()} Miles: ${car3.getMilesDriven().toStringAsFixed(0)} Age: ${car3.getAge()} Total number of cars created: ${Car.numberOfCars}');
+  print('Car 1: ${car1.getBrand()} ${car1.getModel()} ${car1.getYear()} Miles: ${car1.getMilesDriven().toStringAsFixed(0)} Age: ${car1.getAge()}');
+  
+  print( 'Car 2: ${car2.getBrand()} ${car2.getModel()} ${car2.getYear()} Miles: ${car2.getMilesDriven().toStringAsFixed(0)} Age: ${car2.getAge()}');
+  
+  print('Car 3: ${car3.getBrand()} ${car3.getModel()} ${car3.getYear()} Miles: ${car3.getMilesDriven().toStringAsFixed(0)} Age: ${car3.getAge()}');
+
+  print('Total number of cars created: ${Car.numberOfCars}');
 }
